5c6a2a7ac684c28d454b2e7676a8ee82e801cec2
Adam Mellor <melloradam97@gmail.com>
2021-10-06 11:43:05 +0100

    added map stuff

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
-    domains: ['links.papareact.com']
-  }
-}
+    domains: ['links.papareact.com'],
+  },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoibWVsbG9yYWRhbTk3IiwiYSI6ImNrdWZiMWh6YjFzYTQycWw5dzBzeHg0bXMifQ.MWQ-6GXH0MBSJNWTUDdC0Q',
+  },
+};
--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,15 @@
 import { useRouter } from 'next/dist/client/router';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
-import { format, isThisMinute } from 'date-fns';
 import InfoCard from '../components/InfoCard';
+import { format } from 'date-fns';
+import dynamic from 'next/dynamic';
+import Footer from '../components/Footer';
+
+// Client Side Render as we need Global Window Object
+const Map = dynamic(() => import('../components/Map'), {
+  loading: () => 'Loading...',
+  ssr: false,
+});
 
 function Search({ searchResults }) {
   const router = useRouter();
@@ -19,19 +26,16 @@ function Search({ searchResults }) {
       <main className='flex'>
         <section className='flex-grow pt-14 px-6'>
           <p className='text-xs'>
-            300+ Stays - {range} - for {noOfGuests} guests
+            300+ stays - {range} - {noOfGuests} guests{' '}
           </p>
 
           <h1 className='text-3xl font-semibold mt-2 mb-6'>
             Stays in {location}
           </h1>
 
-          <div
-            className='hidden lg:inline-flex mb-5 space-x-3
-          text-gray-800 whitespace-nowrap'
-          >
-            <p className='button'>Cancellation Flexibilty</p>
-            <p className='button'>Type of Place</p>
+          <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+            <p className='button'>Cancellation Flexibility</p>
+            <p className='button'>Type of place</p>
             <p className='button'>Price</p>
             <p className='button'>Rooms and Beds</p>
             <p className='button'>More filters</p>
@@ -53,6 +57,10 @@ function Search({ searchResults }) {
               )
             )}
           </div>
+        </section>
+
+        <section className='hidden xl:inline-flex xl:min-w-[600px]'>
+          <Map searchResults={searchResults} />
         </section>
       </main>
 
